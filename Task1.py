9e2f1dd40328511539ba9930f53f3fba337c2b15
Devang Bhatnagar <gamefun7102@gmail.com>
2024-10-08 10:30:41 -0700

    Finished task 1 frf fr

--- Task1.py
+++ Task1.py
@@ -16,8 +16,8 @@ SECOND_LAST_BYTE = -2
 WRONG_PADDING_NUMBER = 255
 BLOCKSIZE = 16
 
-q = 37 
-a = 5
+q = 0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371
+a = 0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5
 ya = 0
 yb = 0
 c0 = ""
@@ -176,7 +176,7 @@ def bob(q, a):
 	hash_object = hashlib.sha256()
 	hash_object.update(str(s).encode('utf-8'))
 	# print(type(hash_object.digest()))
-	k = hash_object.digest()
+	k = hash_object.digest()[:16]
 	# print(type(k))
 	m1 = "Hi Alice!".encode('utf-8')
 	c1 = cbc_encrypt(m1, k, iv)
@@ -190,7 +190,7 @@ def alice (q, a) :
 	s = pow(yb, xa, q)
 	hash_object = hashlib.sha256()
 	hash_object.update(str(s).encode('utf-8'))
-	k = hash_object.digest()
+	k = hash_object.digest()[:16]
 	m0 = "Hi Bob!".encode('utf-8')
 	c0 = cbc_encrypt(m0, k, iv)
 	print("Decrypted c0: ", cbc_decrypt(c0, k, iv))
